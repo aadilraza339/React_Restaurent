776cd68c2d8dfcc400393b8d314bc8be59b773c6
aadilraza339 <aadil18@navgurukul.org>
2022-01-20 15:26:59 +0530

    Update Dishes_div.js

--- restaurent/src/Dishes_div.js
+++ restaurent/src/Dishes_div.js
@@ -1,20 +1,22 @@
 import React from 'react';
 
-function Dishes(props) {
-return(<>
+function Dishes() {
+    let dishes = [{src:"",menu_name:""}]
+return(
+    <div className="menu__container">
+            {dishes.map((dishe)=>(
+                <div className="down_div">
+                <div className="first_down_div">
 
-<div className="down_div">
-<div className="first_down_div">
-
-    <img 
-    src={props.imgsrc}
-    alt="food-plaza"
-    className="down_img"/>
-    </div>
-    <div className="manu_span"> {props.manu_name} </div>
-</div>
-
-</>
+                    <img 
+                    src={dishe.src}
+                    alt="food-plaza"
+                    className="down_img"/>
+                    </div>
+                    <div className="manu_span"> {dishe.manu_name} </div>
+                </div>
+            ))}
+        </div>
     )
 }
 export default Dishes;
